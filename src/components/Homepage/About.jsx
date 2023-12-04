62c6b51e99440c1ed68ad50c9fab9107be16735a
itsjustchioma <chiomacodes@gmail.com>
2023-12-04 22:51:55 +0100

    bug: fixed the flex issue!

--- src/components/Homepage/About.jsx
+++ src/components/Homepage/About.jsx
@@ -1,28 +1,52 @@
-// About.jsx
 import React from 'react';
-
+import content from './content'; 
 function About(props) {
-    return (
-      <div className='About-Section'>
-        {props.texts ? (
-          <div className='about'>
-            <div className='maincontent'>
-              <h1 className='contenttext'>{props.texts.head}</h1>
-              <h2 className='contentparagraph'>{props.texts.subtext}</h2>
-            </div>
-          </div>
-        ) : (
-          <>
-            <h1 className='aboutus'>About Us</h1>
-            <p className='aboutparagraph'>
-              Radeur is a ground-breaking web that puts the power of network
-              performance assessment directly into your hands. With Radeur, you
-              take control of your internet connection and unleash its full
-              potential.
-            </p>
-          </>
-        )}
+  return (
+    <div>
+      {!props.texts && (
+        <div className='About-Section'>
+          <h1 className='aboutus'>About Us</h1>
+          <p className='aboutparagraph'>
+            Radeur is a ground-breaking web that puts the power of network
+            performance assessment directly into your hands. With Radeur, you
+            take control of your internet connection and unleash its full
+            potential.
+          </p>
+        </div>
+      )}
+
+      {!props.texts && (
+        <div className="About-Main">
+        {!props.texts
+          ? content.map(item => (
+              <div key={item.id} className='About-Section'>
+                <div className='about'>
+                  <div className='maincontent'>
+                    <h1 className='contenttext'>{item.head}</h1>
+                    <h2 className='contentparagraph'>{item.subtext}</h2>
+                  </div>
+                </div>
+              </div>
+            ))
+          : (
+              <div className='about'>
+                <div className='maincontent'>
+                  <h1 className='contenttext'>{props.texts.head}</h1>
+                  <h2 className='contentparagraph'>{props.texts.subtext}</h2>
+                </div>
+              </div>
+            )
+        }
       </div>
-    );
-  }
-  export default About
+      
+      )}
+    </div>
+  );
+}
+
+export default About;
+
+
+
+
+
